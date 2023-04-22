a48f5a3cc33079d4d26bfa9b460b24781cd6c576
Mayank Singla <singlamayank001@gmail.com>
2023-04-22 12:45:40 +0530

    Add exported dataset from actual timetable

--- client/src/algorithms/dataset/timetable/slots.ts
+++ client/src/algorithms/dataset/timetable/slots.ts
@@ -1,112 +1,142 @@
 import Slot from "../../models/slot";
-import { LectureType, WeekDay } from "../../../utils/enums";
+import { WeekDay } from "../../../utils/enums";
 import Interval from "../../models/interval";
 import { SlotAsJSON } from "../../../utils/interfaces";
 
-export const generateSlots = (slotsAsJSON: SlotAsJSON[] = []) =>
-    slotsAsJSON.length === 0
-        ? [
-              new Slot(1, "B1", LectureType.Normal, [
-                  [WeekDay.Monday, new Interval("08:00", "08:50")],
-              ]),
-              new Slot(2, "A", LectureType.Normal, [
-                  [WeekDay.Monday, new Interval("09:00", "09:50")],
-                  [WeekDay.Tuesday, new Interval("17:00", "17:50")],
-                  [WeekDay.Wednesday, new Interval("09:00", "09:50")],
-              ]),
-              new Slot(3, "H", LectureType.Normal, [
-                  [WeekDay.Monday, new Interval("10:00", "10:50")],
-                  [WeekDay.Wednesday, new Interval("11:00", "11:50")],
-                  [WeekDay.Friday, new Interval("08:00", "08:50")],
-              ]),
-              new Slot(4, "D", LectureType.Normal, [
-                  [WeekDay.Monday, new Interval("11:00", "11:50")],
-                  [WeekDay.Wednesday, new Interval("17:00", "17:50")],
-                  [WeekDay.Friday, new Interval("10:00", "10:50")],
-              ]),
-              new Slot(5, "E", LectureType.Normal, [
-                  [WeekDay.Monday, new Interval("12:00", "12:50")],
-                  [WeekDay.Wednesday, new Interval("12:00", "12:50")],
-                  [WeekDay.Friday, new Interval("12:00", "12:50")],
-              ]),
-              new Slot(6, "L", LectureType.Normal, [
-                  [WeekDay.Monday, new Interval("14:00", "15:15")],
-                  [WeekDay.Friday, new Interval("15:30", "16:45")],
-              ]),
-              new Slot(7, "K", LectureType.Normal, [
-                  [WeekDay.Monday, new Interval("15:30", "16:45")],
-                  [WeekDay.Friday, new Interval("14:00", "15:15")],
-              ]),
-              new Slot(8, "C", LectureType.Normal, [
-                  [WeekDay.Monday, new Interval("17:00", "17:50")],
-                  [WeekDay.Wednesday, new Interval("08:00", "08:50")],
-                  [WeekDay.Friday, new Interval("09:00", "09:50")],
-              ]),
-              new Slot(9, "P1", LectureType.Lab, [
-                  [WeekDay.Monday, new Interval("10:00", "12:50")],
-              ]),
-              new Slot(10, "P4", LectureType.Lab, [
-                  [WeekDay.Monday, new Interval("14:00", "16:45")],
-              ]),
-              new Slot(11, "D1", LectureType.Normal, [
-                  [WeekDay.Tuesday, new Interval("08:00", "08:50")],
-              ]),
-              new Slot(12, "F", LectureType.Normal, [
-                  [WeekDay.Tuesday, new Interval("09:00", "10:15")],
-                  [WeekDay.Thursday, new Interval("09:00", "10:15")],
-                  [WeekDay.Friday, new Interval("17:00", "17:50")],
-              ]),
-              new Slot(13, "G", LectureType.Normal, [
-                  [WeekDay.Tuesday, new Interval("10:30", "11:45")],
-                  [WeekDay.Thursday, new Interval("10:30", "11:45")],
-              ]),
-              new Slot(14, "M", LectureType.Normal, [
-                  [WeekDay.Tuesday, new Interval("12:00", "12:50")],
-                  [WeekDay.Thursday, new Interval("12:00", "12:50")],
-              ]),
-              new Slot(15, "I", LectureType.Normal, [
-                  [WeekDay.Tuesday, new Interval("14:00", "15:15")],
-                  [WeekDay.Thursday, new Interval("15:30", "16:45")],
-              ]),
-              new Slot(16, "J", LectureType.Normal, [
-                  [WeekDay.Tuesday, new Interval("15:30", "16:45")],
-                  [WeekDay.Thursday, new Interval("14:00", "15:15")],
-              ]),
-              new Slot(17, "P5", LectureType.Lab, [
-                  [WeekDay.Tuesday, new Interval("14:00", "16:45")],
-              ]),
-              new Slot(18, "B", LectureType.Normal, [
-                  [WeekDay.Wednesday, new Interval("10:00", "10:50")],
-                  [WeekDay.Thursday, new Interval("17:00", "17:50")],
-                  [WeekDay.Friday, new Interval("11:00", "11:50")],
-              ]),
-              new Slot(19, "C1", LectureType.Normal, [
-                  [WeekDay.Wednesday, new Interval("14:00", "14:50")],
-              ]),
-              new Slot(20, "P2", LectureType.Lab, [
-                  [WeekDay.Wednesday, new Interval("10:00", "12:50")],
-              ]),
-              new Slot(21, "A1", LectureType.Normal, [
-                  [WeekDay.Thursday, new Interval("08:00", "08:50")],
-              ]),
-              new Slot(22, "P6", LectureType.Lab, [
-                  [WeekDay.Thursday, new Interval("14:00", "16:45")],
-              ]),
-              new Slot(23, "P3", LectureType.Lab, [
-                  [WeekDay.Friday, new Interval("10:00", "12:50")],
-              ]),
-              new Slot(24, "P7", LectureType.Lab, [
-                  [WeekDay.Friday, new Interval("14:00", "16:45")],
-              ]),
-          ]
-        : slotsAsJSON.map((slot, idx) => {
-              return new Slot(
-                  idx,
-                  slot.name,
-                  slot.lectureType,
-                  slot.dayTime.map((dt) => [
-                      dt[0] as WeekDay,
-                      new Interval(dt[1].start, dt[1].end),
-                  ])
-              );
-          });
+const getDefaultSlotsAsJSON = (): SlotAsJSON[] => [
+    {
+        name: "B1",
+        lectureType: "Normal",
+        dayTime: [["Monday", { start: "08:00", end: "08:50" }]],
+    },
+    {
+        name: "A",
+        lectureType: "Normal",
+        dayTime: [
+            ["Monday", { start: "09:00", end: "09:50" }],
+            ["Tuesday", { start: "17:00", end: "17:50" }],
+            ["Wednesday", { start: "09:00", end: "09:50" }],
+        ],
+    },
+    {
+        name: "H",
+        lectureType: "Normal",
+        dayTime: [
+            ["Monday", { start: "10:00", end: "10:50" }],
+            ["Wednesday", { start: "11:00", end: "11:50" }],
+            ["Friday", { start: "08:00", end: "08:50" }],
+        ],
+    },
+    {
+        name: "D",
+        lectureType: "Normal",
+        dayTime: [
+            ["Monday", { start: "11:00", end: "11:50" }],
+            ["Wednesday", { start: "17:00", end: "17:50" }],
+            ["Friday", { start: "10:00", end: "10:50" }],
+        ],
+    },
+    {
+        name: "E",
+        lectureType: "Normal",
+        dayTime: [
+            ["Monday", { start: "12:00", end: "12:50" }],
+            ["Wednesday", { start: "12:00", end: "12:50" }],
+            ["Friday", { start: "12:00", end: "12:50" }],
+        ],
+    },
+    {
+        name: "C",
+        lectureType: "Normal",
+        dayTime: [
+            ["Monday", { start: "17:00", end: "17:50" }],
+            ["Wednesday", { start: "08:00", end: "08:50" }],
+            ["Friday", { start: "09:00", end: "09:50" }],
+        ],
+    },
+    {
+        name: "P1",
+        lectureType: "Lab",
+        dayTime: [["Monday", { start: "14:00", end: "16:45" }]],
+    },
+    {
+        name: "D1",
+        lectureType: "Normal",
+        dayTime: [["Tuesday", { start: "08:00", end: "08:50" }]],
+    },
+    {
+        name: "F",
+        lectureType: "Normal",
+        dayTime: [
+            ["Tuesday", { start: "09:00", end: "10:15" }],
+            ["Thursday", { start: "09:00", end: "10:15" }],
+            ["Friday", { start: "17:00", end: "17:50" }],
+        ],
+    },
+    {
+        name: "G",
+        lectureType: "Normal",
+        dayTime: [
+            ["Tuesday", { start: "10:30", end: "11:45" }],
+            ["Thursday", { start: "10:30", end: "11:45" }],
+        ],
+    },
+    {
+        name: "M",
+        lectureType: "Normal",
+        dayTime: [
+            ["Tuesday", { start: "12:00", end: "12:50" }],
+            ["Thursday", { start: "12:00", end: "12:50" }],
+        ],
+    },
+    {
+        name: "P2",
+        lectureType: "Lab",
+        dayTime: [["Tuesday", { start: "14:00", end: "16:45" }]],
+    },
+    {
+        name: "B",
+        lectureType: "Normal",
+        dayTime: [
+            ["Wednesday", { start: "10:00", end: "10:50" }],
+            ["Thursday", { start: "17:00", end: "17:50" }],
+            ["Friday", { start: "11:00", end: "11:50" }],
+        ],
+    },
+    {
+        name: "P3",
+        lectureType: "Lab",
+        dayTime: [["Wednesday", { start: "14:00", end: "16:45" }]],
+    },
+    {
+        name: "A1",
+        lectureType: "Normal",
+        dayTime: [["Thursday", { start: "08:00", end: "08:50" }]],
+    },
+    {
+        name: "P4",
+        lectureType: "Lab",
+        dayTime: [["Thursday", { start: "14:00", end: "16:45" }]],
+    },
+    {
+        name: "P5",
+        lectureType: "Lab",
+        dayTime: [["Friday", { start: "14:00", end: "16:45" }]],
+    },
+];
+
+export const generateSlots = (
+    slotsAsJSON: SlotAsJSON[] = getDefaultSlotsAsJSON()
+) => {
+    return slotsAsJSON.map((slot, idx) => {
+        return new Slot(
+            idx,
+            slot.name,
+            slot.lectureType,
+            slot.dayTime.map((dt) => [
+                dt[0] as WeekDay,
+                new Interval(dt[1].start, dt[1].end),
+            ])
+        );
+    });
+};
